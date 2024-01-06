e6b02635b72ceea0c4b7dfd38c5c762fad149127
Gwon Junhyeok <108499717+jh10253267@users.noreply.github.com>
2024-01-06 15:28:16 +0900

    init commit

---
+++ src/main/java/com/example/awstest/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.awstest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "main";
+    }
+}
